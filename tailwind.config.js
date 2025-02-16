bed3648f6774b74bc0efa9029f219e4a29c91c9a
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-16 18:22:26 -0300

    chore: add tailwind configs

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#909090",
+        "primary-bright": "#BBBBBB",
+        secondary: "#101010",
+        "bg-primary": "#D9D9D9",
+        "bg-secondary": "#BCBCBC",
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+};
