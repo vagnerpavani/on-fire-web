8a8e3662abc6fed6b90bb218d9d23a84a6dbb3ad
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-17 18:13:43 -0300

    refactor: change props type

--- src/components/base/PageWrapper.tsx
+++ src/components/base/PageWrapper.tsx
@@ -1,9 +1,9 @@
 import { JSX } from "react";
 
-type PageWrapperProps = { children: JSX.Element };
+type PageWrapperProps = { children: JSX.Element[] | JSX.Element };
 
 function PageWrapper({ children }: PageWrapperProps) {
-  return <div className="min-h-screen">{children}</div>;
+  return <div className="min-h-screen ">{children}</div>;
 }
 
 export default PageWrapper;
