aca96bddc6a2e12140a846153f8307301d1e9332
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-17 20:51:32 -0300

    feat[ui]: add dashboard line chart component

---
+++ src/pages/dashboard/components/structure/DashboardLineChart.tsx
@@ -0,0 +1,55 @@
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type DataItem = {
+  name: string;
+} & {
+  [key: string]: number | string;
+} & Omit<{ name: string }, "name">;
+
+type Props = {
+  data: DataItem[];
+  colors: { [lineName: string]: string };
+};
+function DashboardLineChart({ data, colors }: Props) {
+  const Lines = () => {
+    const lines = Object.keys(data[0]);
+    return lines.map((line, i) => {
+      console.log(line);
+      return line === "name" ? (
+        ""
+      ) : (
+        <Line
+          type="monotone"
+          dataKey={line}
+          stroke={colors[line]}
+          key={i}
+          activeDot={{ r: 8 }}
+        />
+      );
+    });
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart data={data}>
+        <CartesianGrid />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {Lines()}
+      </LineChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default DashboardLineChart;
