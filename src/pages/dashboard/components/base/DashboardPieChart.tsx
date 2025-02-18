dada94d08426b451e84dca3b2792aa1ada08e38a
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-18 14:39:21 -0300

    refactor: remove type declaration

---
+++ src/pages/dashboard/components/base/DashboardPieChart.tsx
@@ -0,0 +1,29 @@
+import { Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+
+type Props = {
+  data: { name: string; value: number }[];
+  color: string;
+};
+
+function DashboardPieChart({ data, color }: Props) {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie
+          dataKey="value"
+          isAnimationActive={true}
+          data={data}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill={color}
+          label
+        />
+
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default DashboardPieChart;
