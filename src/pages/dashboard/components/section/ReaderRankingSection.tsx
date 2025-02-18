acc6f2270538e897cc726122153ca23913cddcee
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-18 15:41:14 -0300

    feat[ui]: add traffic and reader sections

---
+++ src/pages/dashboard/components/section/ReaderRankingSection.tsx
@@ -0,0 +1,20 @@
+import Reader from "../base/Reader";
+
+function ReaderRankingSection() {
+  return (
+    <div className="p-2 bg-brand-white  rounded-md my-2 border-solid border-2 w-fit border-gray-100">
+      <h2 className="text-2xl font-bold text-brand-yellow">
+        Leitores mais engajados:
+      </h2>
+      <div className="h-48 overflow-auto">
+        <Reader isKing={true} email={"fulano@email.com"} position={1} />
+        <Reader isKing={false} email={"fulano2@email.com"} position={2} />
+        <Reader isKing={false} email={"fulano3@email.com"} position={3} />
+        <Reader isKing={false} email={"fulano4@email.com"} position={4} />
+        <Reader isKing={false} email={"fulano5@email.com"} position={5} />
+      </div>
+    </div>
+  );
+}
+
+export default ReaderRankingSection;
---
+++ src/pages/dashboard/components/section/TrafficStatsSection.tsx
@@ -0,0 +1,37 @@
+import ReaderBarChart from "../structure/ReaderBarChart";
+import SourcePieChart from "../structure/SourcePieChart";
+
+function TrafficStatsSection() {
+  const pieDataMock = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+  ];
+  const barDataMock = [
+    {
+      name: "C1",
+      engagement: 4000,
+    },
+    {
+      name: "C2",
+      engagement: 3000,
+    },
+    {
+      name: "C3",
+      engagement: 2000,
+    },
+  ];
+  return (
+    <div className="p-2 bg-brand-white  rounded-md  my-2 border-solid border-2 border-gray-100">
+      <h2 className="text-2xl font-bold text-brand-yellow ">
+        Estátisticas de tráfego:
+      </h2>
+      <div className="flex justify-around">
+        <SourcePieChart data={pieDataMock} />
+        <ReaderBarChart data={barDataMock} />
+      </div>
+    </div>
+  );
+}
+
+export default TrafficStatsSection;
