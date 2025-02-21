dae9e49931d224fcf7bbfef877b8e91d5a6ec55a
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-21 19:46:59 -0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import DashboardPage from "./pages/dashboard/DashboardPage";
 import StreakPage from "./pages/streak/StreakPage";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 function App() {
   return (
-    <>
-      <StreakPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StreakPage />} />
+        <Route path="/admin/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
