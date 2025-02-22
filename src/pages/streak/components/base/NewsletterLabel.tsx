b86078d2d722c5ca589d492c079df50593c529ce
vagnerpavani <vagner.alves.pavani@gmail.com>
2025-02-22 16:04:13 -0300

    feat: add display user history

--- src/pages/streak/components/base/NewsletterLabel.tsx
+++ src/pages/streak/components/base/NewsletterLabel.tsx
@@ -1,12 +1,12 @@
 type Props = {
-  publishedAt: string;
+  title: string;
   publishedDay: string;
 };
 
-function NewsletterLabel({ publishedAt, publishedDay }: Props) {
+function NewsletterLabel({ title, publishedDay }: Props) {
   return (
     <div className="bg-brand-alt-yellow w-96 p-2 flex flex-col items-start justify-center rounded-xl my-2">
-      <p className="font-sans-alt">{publishedAt}</p>
+      <p className="font-sans-alt">{title}</p>
       <p className="font-sans-alt font-semibold">{publishedDay}</p>
     </div>
   );
--- src/pages/streak/components/section/HistorySection.tsx
+++ src/pages/streak/components/section/HistorySection.tsx
@@ -1,13 +1,31 @@
+import { useContext, useEffect } from "react";
 import NewslettersDisplay from "../structure/NewslettersDisplay";
+import { UserContext } from "../../../../context/UserContext";
+import { makeMainApiHttpClient } from "../../../../services/http-client";
 
-function HistorySection() {
+type Props = {
+  setPost: React.Dispatch<React.SetStateAction<Post[]>>;
+  posts: Post[];
+};
+
+function HistorySection({ posts, setPost }: Props) {
+  const [user] = useContext(UserContext);
+  useEffect(() => {
+    if (!user || !user.id) return;
+
+    const apiClient = makeMainApiHttpClient();
+
+    apiClient.get(`/user/${user.id}/posts`).then((res) => {
+      setPost(res.data);
+    });
+  }, [user]);
   return (
     <div className="flex flex-col items-center h-fit bg-brand-white">
       <div className="w-96">
         <h1 className="font-bold text-brand-yellow text-2xl">Seu histórico</h1>
       </div>
 
-      <NewslettersDisplay />
+      <NewslettersDisplay posts={posts} />
     </div>
   );
 }
